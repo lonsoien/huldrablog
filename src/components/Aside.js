0f189e15c956d2b5c0674bb099c041ef12e82b42
lonsoien <79151322+lonsoien@users.noreply.github.com>
2023-08-14 21:08:02 +0200

    media queries

---
+++ src/components/Aside.js
@@ -0,0 +1,29 @@
+import selfie from "../images/tegning-louie.JPG";
+import loreen from "../images/loreen.jpeg";
+
+export default function Aside() {
+    return (
+        <>
+        <aside>
+        <nav>navigation here</nav>
+        <img src={selfie} alt="selfie louisebellingmo drawing" style={{width: "200px", height: "215px", borderRadius: "50%"}}></img>
+        <button>About</button>
+        <h2>I am Louise Bellingmo</h2>
+        <p>This is my blog about lifestyle and my own pictures from nature.</p>
+        <br/>
+        <p>This is my blog and more info will be added here in this text. </p>
+        <br/>
+        <h2>Instagram</h2>
+        <hr/>
+        <h2>images from instagram here</h2>
+        <img src={loreen} alt="loreen tattoo" style={{width: "250px", height: "200px"}}></img>
+        <br/>
+        <button>Contact</button>
+        <br/>
+        <h2>Link to Portfolio</h2>
+        <p>A short video imported here</p>
+        <br/>
+        </aside>
+        </>
+    )
+}
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import createClient from "../client"
-import selfie from "../images/tegning-louie.JPG";
-import loreen from "../images/loreen.jpeg";
+import Aside from "./Aside"; 
+
 
 export default function Blog() {
   const [posts, setPosts] = useState([])
@@ -29,28 +29,7 @@ export default function Blog() {
      <div className="banner"></div>
      <h2>Blog page</h2>
      </header>
-     
-     <aside>
-        <nav>navigation here</nav>
-        <img src={selfie} alt="selfie louisebellingmo drawing" style={{width: "300px", height: "300px", borderRadius: "50%"}}></img>
-        <button>About</button>
-        <h2>I am Louise Bellingmo</h2>
-        <p>This is my blog about lifestyle and my own pictures from nature.</p>
-        <br/>
-        <p>This is my blog and more info will be added here in this text. </p>
-        <br/>
-        <h2>Instagram</h2>
-        <hr/>
-        <h2>images from instagram here</h2>
-        <img src={loreen} alt="loreen tattoo" style={{width: "250px", height: "200px"}}></img>
-        <br/>
-        <button>Contact</button>
-        <br/>
-        <h2>Link to Portfolio</h2>
-        <p>A short video imported here</p>
-        <br/>
-        </aside>
-       
+       <Aside/>
         <main>
           {posts.map((post) => (
             <article key={post.slug.current}>
