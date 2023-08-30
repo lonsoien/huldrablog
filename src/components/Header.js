a81ef3c82add6211e8f3e3980353d6e8e096ed91
lonsoien <loubelons@gmail.com>
2023-08-30 13:04:51 +0200

    banner text

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,10 @@ export default function Header() {
      <div className="banner">
      <img src={banner} alt="huldrablog banner" />
      <div className="huldrablog">Huldrablog</div>
+     <div className="every">EVERY</div>
+     <div className="picture">PICTURE</div>
+     <div className="hasa">HAS A</div>
+     <div className="story">STORY</div>
      </div>
      <h1>by Louise</h1>
      </header>
